5a03dde040d95b0783c757785267e51fed015cc7
sumeshi <j15322sn@gmail.com>
2021-08-13 08:40:18 +0900

    feat: support multiprocess

--- src/ntfsfind.py
+++ src/ntfsfind.py
@@ -3,14 +3,27 @@ import re
 import argparse
 from pathlib import Path
 from typing import List
+from multiprocessing import Pool, cpu_count
 
 from ntfsdump import ImageFile
 from mft import PyMftParser
 
 
-def gen_names(mft: bytes) -> List[str]:
+def extract_name(line: bytes):
+    return line.decode("utf8").split(",")[-1].strip()
+
+
+def gen_names(mft: bytes, multiprocess: bool) -> List[str]:
     csvparser = PyMftParser(io.BytesIO(mft))
-    return [c.decode("utf8").split(",")[-1].strip() for c in csvparser.entries_csv()]
+    if multiprocess:
+        with Pool(cpu_count()) as pool:
+            results = pool.map_async(
+                extract_name,
+                csvparser.entries_csv()
+            )
+            return results.get(timeout=None)
+    else:
+        return [c.decode("utf8").split(",")[-1].strip() for c in csvparser.entries_csv()]
 
 
 def ntfsfind():
@@ -20,20 +33,15 @@ def ntfsfind():
         "search_query", type=str, help="Target File Name (regex is worked.).",
     )
     parser.add_argument("imagefile_path", type=Path, help="raw image file")
-    parser.add_argument(
-        "--volume-num",
-        "-n",
-        type=int,
-        default=None,
-        help="NTFS volume number(default: autodetect).",
-    )
+    parser.add_argument("--volume-num", "-n", type=int, default=None, help="NTFS volume number(default: autodetect).",)
+    parser.add_argument("--multiprocess", "-m", action='store_true', help="flag to run multiprocessing.")
     args = parser.parse_args()
 
     image = ImageFile(args.imagefile_path, args.volume_num)
 
     mft = image.main_volume._NtfsVolume__read_file('/$MFT')
     pattern = re.compile(args.search_query.strip('/'))
-    found_records = [i for i in gen_names(mft) if re.match(pattern, i)]
+    found_records = [i for i in gen_names(mft, args.multiprocess) if re.match(pattern, i)]
     for record in found_records:
         print(record)
 
